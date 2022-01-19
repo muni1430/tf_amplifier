4a675e09e349edfe2d1ff0dd9d66776ea4b54cb4
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-19 17:53:10 +0530

    Update amplifer.tf

--- amplifer.tf
+++ amplifer.tf
@@ -1,18 +1,15 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_amplify_app" "example3" {
-  name       = "example3"
-  repository = "https://github.com/Gunamoni11/tf_amplifier.git"
-  #access_token = "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"
 
-  
-  data.aws_ssm_parameter.foo.value  
-  
 data "aws_ssm_parameter" "foo" {
   name = "/github/access_token"
 }
 
+resource "aws_amplify_app" "example3" {
+  name       = "example3"
+  repository = "https://github.com/Gunamoni11/tf_amplifier.git"
+  access_token = "data.aws_ssm_parameter.foo.value"
 
   # The default build_spec added by the Amplify Console for React.
   build_spec = <<-EOT
