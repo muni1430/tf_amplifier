0f683f34ec48ed5f5067e565cce9d1c7652c33f6
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-20 09:36:57 +0530

    Update amplifer.tf

--- amplifer.tf
+++ amplifer.tf
@@ -7,7 +7,7 @@ data "aws_ssm_parameter" "foo" {
 }
 
 resource "aws_amplify_app" "example3" {
-  name       = "example3"
+  name       = "new one amplify"
   repository = "https://github.com/muni1430/tf_amplifier.git"
   access_token = data.aws_ssm_parameter.foo.value
 
